b7e9821ffd05cba278ca55de8e6163fff758c70a
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-06-02 16:32:29 +0300

    validation

--- TimeSheets.Datalayer.CouchDb/Queries/CountByCallOffOrderIdQuery.cs
+++ TimeSheets.Datalayer.CouchDb/Queries/CountByCallOffOrderIdQuery.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
-using Cmas.DataLayers.CouchDb.TimeSheets.Dtos;
 using Cmas.Infrastructure.Domain.Queries;
 using CouchRequest = MyCouch.Requests;
 using Cmas.BusinessLayers.TimeSheets.Criteria;
@@ -12,36 +11,35 @@ using System;
 
 namespace Cmas.DataLayers.CouchDb.TimeSheets.Queries
 {
-    public class FindByCallOffOrderIdQuery : IQuery<FindByCallOffOrderId, Task<IEnumerable<TimeSheet>>>
+    public class CountByCallOffOrderIdQuery : IQuery<FindByCallOffOrderId, Task<int>>
     {
-        private readonly IMapper _autoMapper;
         private readonly CouchWrapper _couchWrapper;
 
-        public FindByCallOffOrderIdQuery(IServiceProvider serviceProvider)
+        public CountByCallOffOrderIdQuery(IServiceProvider serviceProvider)
         {
-            _autoMapper = (IMapper)serviceProvider.GetService(typeof(IMapper));
-
             _couchWrapper = new CouchWrapper(serviceProvider, DbConsts.ServiceName);
         }
 
-        public async Task<IEnumerable<TimeSheet>> Ask(FindByCallOffOrderId criterion)
+        public async Task<int> Ask(FindByCallOffOrderId criterion)
         {
             var result = new List<TimeSheet>();
             var query =
                 new CouchRequest.QueryViewRequest(DbConsts.DesignDocumentName, DbConsts.ByCallOffOrderDocsViewName)
-                    .Configure(q => q.Key(criterion.CallOffOrderId).Reduce(false));
+                    .Configure(q => q.Key(criterion.CallOffOrderId).Reduce(true));
 
             var viewResult = await _couchWrapper.GetResponseAsync(async (client) =>
             {
-                return await client.Views.QueryAsync<TimeSheetDto>(query);
+                return await client.Views.QueryAsync<int>(query);
             });
 
-            foreach (var row in viewResult.Rows.OrderByDescending(s => s.Value.CreatedAt))
-            {
-                result.Add(_autoMapper.Map<TimeSheet>(row.Value));
-            }
+            var row = viewResult.Rows.FirstOrDefault();
 
-            return result;
+            if (row == null)
+                return 0;
+            else
+            {
+                return row.Value;
+            }
         }
     }
 }
